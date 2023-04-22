91bb8ba21286d323db79aed0abb736bd86828625
Touathi Vang <Touathi@outlook.com>
2023-04-21 20:40:02 -0500

    Looked at my mistakes and was able to understand why and fixed them

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -29,7 +29,7 @@ if (number >= 2) {
 // Make a variable call name and set it as 'Dane'
 // Then we check if name is 'Mary' and if it is 'mary', console log 'Hi, Mary!'
 // If name is not 'Mary' then console log 'How do you do?'
-//
+// Variable is Dane and not Mary so we console.log 'How do you do?'
 
 //CODE
 /*
@@ -45,11 +45,14 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// Make a variable secret but dont set a vaule for it.
-// Make another variable call code and set the value to 123.
-// Check if the code equals 123 then the variable secret will have a value of super and also code will be multiplied by 2 which will come out as 246
-// Then check if the value for code is smaller than 250, secret when will have value of duper
-// we then console log secret for it to show up on the web console.
+// make a variable called secret and end it with a semicolon so that we can set a
+// value to it later, make another variable call code and set the value to it as 123.
+// the code runs, if variable code = '123' then the variable secret will now have the
+// value of 'super' and the value for code, '123' will be multiply by 2 which is now 246.
+// then the code wants to know if the value for variable code is bigger than 250.
+// If it is then the variable for secret will now be changed to 'duper',
+// then console.log secret which the value will stay super because 246 is smaller than 250.
+
 
 //CODE
 /*
@@ -71,11 +74,14 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// Make a boolean called isStudent and set it as true, and make variale age and zip and have the value as 34, 55407
-// The first thing we check is if isStudent is true and if zip is smaller than 80000 then console log 'You're a student on the West Coast!'
-// If the first check wasn't true then check if isStudent is false or if age is smaller then 30 and console log 'What are your hobbies?'
-// Check if isStudent is true then console log 'Welcome to Prime!'
-// If none of those checks are true then console log 'How about the weather?'
+// Make a boolean called isStudent and set it as true, and make variale age and zip
+// and have the value as 34, and 55407. The first thing we check is if isStudent is
+// true and if zip is bigger than 80000 then console log 'You're a student on the West Coast!'
+// The else if is if, 'isStudent' === false or age is smaller than 30 console.log 'What are your hobbies?'
+// The 2nd else if is if, 'isStudent === true then console.log 'Welcome to Prime!'
+// else is if the first three if does not match then console.log 'How about the weather?'
+// When running this code, it will enter the 2nd else if because 'isStudent' === true 
+//and the console.log 'Welcome to Prime!' will show up.
 
 //CODE
 /*
@@ -94,8 +100,6 @@ if (isStudent === true && zip > 80000 ) {
 }
 
 */
-
-
 
 
 // REVERSE
@@ -131,7 +135,9 @@ if (number >= 2) {
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
-// FIX -- Fix colorOne to blue and colorTwo to red and then in the conditional add in colorTwo === 'purple'; and add == to colorOne = 'purple' for it to be ===.
+// FIX - First change the value of colorOne to 'blue', and the value of colorTwo to 'red'.
+// The code runs, if Mix = true then colorOne and colorTwo is 'purple', we need to add colorTwo = 'purple' under line 'colorTwo = 'purple';'
+// So mix is true, then the value of colorOne and colorTwo is 'purple'.
 
 /*
 let colorOne = 'red';
@@ -140,6 +146,7 @@ let mix = true;
 
 if (mix === true) {
   colorOne = 'purple';
+// (FIX - colorTwo = 'purple';)
 }
 */
 
